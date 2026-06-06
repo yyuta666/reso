32a077c06dbcb4225059536ad312f22fa5b16938
yyuta666 <yyuta074@gmail.com>
2026-06-07 00:21:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ from aiogram.filters import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [8381286547]  # ЗАМЕНИ НА СВОЙ user_id
+ADMIN_IDS = [8381286547] GREETING = "Привет! Выбери раздел:"
+
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
